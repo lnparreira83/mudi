0c67630aa51a904ff878ff30d408345f1db32828
Lucas Parreira <lnparreira83@gmail.com>
2021-01-17 13:58:52 +1300

    adicionando arquivos do projeto

---
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,39 @@
+package br.com.alura.mvc.mudi.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import br.com.alura.mvc.mudi.model.StatusPedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	@GetMapping
+	public String home(Model model, Principal principal) {
+
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 5,sort);
+
+		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE,paginacao);
+		model.addAttribute("pedidos", pedidos);
+
+		return "home";
+	}
+
+}
